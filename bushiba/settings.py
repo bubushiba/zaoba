f5da160740b9afca1588a00e47432ab3cfbcd6ff
bushiba <16848972+bubushiba@user.noreply.gitee.com>
2026-05-14 22:37:10 +0800

    添加部署文件

--- bushiba/settings.py
+++ bushiba/settings.py
@@ -27,9 +27,9 @@ MEDIA_URL = '/media/'       # 图片，文档都保存在这个文件夹下
 SECRET_KEY = 'django-insecure-kbu7(9it01#b!2uc=0)pi58dd%#*_qod)zligk0r=6o^)f(9ci'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
